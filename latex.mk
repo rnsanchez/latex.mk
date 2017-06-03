25f6f7393f0ce02651909f0a2508113f2efc375c
Ricardo Nabinger Sanchez <rnsanchez@wait4.org>
2017-06-02 23:01:34 -0300

    Remove $Id$, update Copyright year.

--- latex.mk
+++ latex.mk
@@ -1,5 +1,5 @@
 #
-# Copyright © 2006--2009  Ricardo Nabinger Sanchez <rnsanchez@wait4.org>
+# Copyright © 2006--2017  Ricardo Nabinger Sanchez <rnsanchez@wait4.org>
 # All rights reserved.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
@@ -13,8 +13,6 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-#
-# $Id$
 #
 
 
